266473c8413880a0a5269ede459d67e7fdeb74dc
aier9500 <aierjjwu8@gmail.com>
2024-06-15 14:29:39 -0400

    added some files getting started

---
+++ Conversion.java
@@ -0,0 +1,13 @@
+public class Conversion {
+    
+    private String from;
+    private String to; 
+    private String result;
+    private String additionalNotes;
+
+    public Conversion(String from, String to) {
+
+        this.from = from;
+        this.to = to;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String arg[]) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Transposing FROM: ");
+        String sourceScale = scan.nextLine();
+        
+        System.out.println("Transposing TO: ");
+        String destinationScale = scan.nextLine();
+
+        Conversion.
+        
+
+
+
+        scan.close();
+    }
+}
